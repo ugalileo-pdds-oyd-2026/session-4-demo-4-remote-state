c7b1c28a993b78d36ab2ed501211c7cc24fb3a22
Tito Alvarez <augustoalvarez@gmail.com>
2026-05-14 20:44:41 -0600

    Lock demo

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "workspace_demo" {
   bucket = "acme-workspace-demo-${var.environment}"
   tags   = { Environment = var.environment }
 }
+
+resource "time_sleep" "demo_lock" {
+  create_duration = "30s"
+  depends_on      = [aws_s3_bucket.workspace_demo]
+}
