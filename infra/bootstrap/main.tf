21140289157179ab935f58a6cfc5d0d7b6036848
Tito Alvarez <augustoalvarez@gmail.com>
2026-05-14 20:29:42 -0600

    Fixing variables

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "state" {
-  bucket = "${var.team_name}-tfstate-${var.environment}"
+  bucket = "${var.team_name}-tfstate"
   lifecycle { prevent_destroy = true }
 }
 
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_versioning" "state" {
 }
 
 resource "aws_dynamodb_table" "locks" {
-  name         = "${var.team_name}-terraform-locks-${var.environment}"
+  name         = "${var.team_name}-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -2,11 +2,6 @@ variable "team_name" {
   type        = string
   description = "Name of the team"
 }
-variable "environment" {
-  type        = string
-  default     = "shared"
-  description = "Environment name"
-}
 variable "region" {
   type        = string
   default     = "us-west-2"
